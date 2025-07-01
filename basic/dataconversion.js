4c19e1ad47678a952e2176613c50c7f1ee58c6a9
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-01 11:34:11 +0000

    data conversion

---
+++ basic/dataconversion.js
@@ -0,0 +1,9 @@
+let card_number=23
+
+let car_detailed="this is detailed for the card "
+console.log(typeof car_detailed);
+console.log(typeof card_number);
+
+let convrsion = Number(car_detailed)
+console.log(typeof car_detailed);
+console.log(typeof card_number);
