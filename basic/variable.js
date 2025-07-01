3bd7557c0c0e0ced80e1de68f323521c34980892
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-01 07:41:52 +0000

    this is for variable

---
+++ basic/variable.js
@@ -0,0 +1,17 @@
+// introduction for variable
+
+const account_id=21332
+// let is mostly use in variable declration because of its significance scope ability insted of var.
+let account_name= "ecommerce"
+
+var account_email= "table@gmail.com"
+account_pro="this is not  promotaed method for declare the variable"
+
+console.log(account_id);
+console.log(account_name);
+console.log(account_email);
+console.log(account_pro);
+
+// using console.table for print two or more data with tabular form
+console.table([account_id,account_email,account_name,account_pro])
+
