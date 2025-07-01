9605cf650d1ea6a0c4b5ecd07339b675ab551af7
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-01 09:42:10 +0000

    this is for datatype

---
+++ basic/Datatype.js
@@ -0,0 +1,29 @@
+"use strict" // treat all the js code as newer  version
+ 
+// alert(4+3) // we are using nodejs not browser so it is not run 
+
+let name ="suru"
+let age=23
+let boolean =true 
+
+//types of data type
+/*
+Integer
+bigint
+string
+boolean 
+undefine -> when we not initialize the value of variable
+null -> its represent the empty value , standalone value
+ 
+symbol ->unique
+
+object
+*/
+
+console.log(typeof suru);
+console.log(typeof age);
+console.log(typeof "this is for us");
+console.log(typeof true);
+console.log(typeof null); // object 
+console.log(typeof undefined) //undefined
+
