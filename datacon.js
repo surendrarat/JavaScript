3836d7acdc5b2f6505c88b2db350fd7ff29dbc97
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-02 17:23:31 +0000

    this is for datatype conversion

---
+++ datacon.js
@@ -0,0 +1,40 @@
+console.log("we are study about how the  data is convert from one datatype to another data type");
+
+
+// let age = "32aes";
+
+// let ok = "suru";
+
+// console.log(typeof age);
+// console.log(typeof ok);
+
+// console.log(age);
+// console.log(ok);
+
+
+// let here = Number(age);
+// console.log(typeof here);
+// console.log(here);
+
+//  let go=true;
+//   go=false;
+//  let same=Number(go);
+//  console.log(same); 
+
+//   same=String(go);
+//  console.log(same); 
+
+ let harry="hello";
+ let make=Boolean(harry);
+ console.log(make);
+ let game="";
+ let pro=Boolean(game);
+ console.log(pro);
+ 
+ 
+// in many case when we are tyring to change the string into number then unuswal sting is not cosider and show the NaN (not a number)
+/* "33" => 33
+"33sdg" => NaN
+ture=> 1 false=>0
+*/
+
