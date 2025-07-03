43c039b6532e87a3f98ac898d5be23167f610070
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-03 18:50:38 +0000

    this is for operator(arithematic and logical)

---
+++ basic/hello.js
@@ -0,0 +1,27 @@
+console.log("hello")
+// arthimatic operation
+let a=32;
+let b=13;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+// logical operation
+let g=32;
+let e=25;
+let m=32;
+let p=23;
+
+if (g>e&&m>p) {
+    console.log(g ,"=", m);
+    
+}
+if (g=m||m<p) {
+    console.log(g);
+    
+}
+if (g>e!=m>p) {
+    console.log(p);
+    
+}
