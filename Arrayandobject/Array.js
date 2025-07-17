bcaeae9c15f933556667c322bd623c775d928396
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-17 17:45:57 +0000

    this is simple

---
+++ Arrayandobject/Array.js
@@ -0,0 +1,18 @@
+// we can store theh multiple type value
+let firstArr=["suru" ,"rath", 1 , true];
+// remember the position of the vlaue because we are use the index for access or  find out these .
+// not have key for value 
+console.log(typeof firstArr[0]);
+console.log(typeof firstArr[1]);
+console.log(typeof firstArr[2]);
+console.log(typeof firstArr[3]);
+ 
+let secondArr=["start" ,23]
+secondArr[1]
+ 
+
+// the best practice of the declare the array is the using the const keyword 
+// because it is provide abiliy of not changing referenc of array but change it's element or values
+const 
+
+
