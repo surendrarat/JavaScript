27bd0ff0469be840f4aedf8def3c6c5138685f4f
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-18 16:44:47 +0000

    implisite conversion by javascript

---
+++ typeCasting/implisite.js
@@ -0,0 +1,14 @@
+let one="hello";
+let two=2;
+// the implesite converstion is not a good think in any programming
+//implesite typecasting for javascript 
+console.log(2+'2');//string + number =stirng by javascript
+console.log(true+3);//boolean + number=number by javascript it's treate boolean like a number (0 and 1)
+
+console.log(true+'2');//boolean+string=string-> true2
+console.log(null+'3'); //null+number=number , null+string=string
+console.log(true+false);//boolean +boolean =number
+console.log(Object+2);//function Object() { [native code] }2
+console.log(Object+Object)
+
+//
