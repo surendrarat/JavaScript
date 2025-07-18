797b83613790a1b9fd0354edd44100343583de48
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-18 17:08:27 +0000

    Charecter finding in string

---
+++ String/find_charecter.js
@@ -0,0 +1,7 @@
+let village="patliya"
+//using the bracket notation for finding the charecter in a string
+console.log(village[1]);
+
+//last charecter
+console.log(village[village.length-1]);
+console.log(village[2]+village[4])
