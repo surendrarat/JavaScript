62247e40af4fe601eedd685ae174901513d550dd
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-18 17:24:31 +0000

    excape charecter and templete literals

---
+++ String/escapechara.js
@@ -0,0 +1,19 @@
+console.log("hello this,\n is sting");//for new line
+
+//escape some special or like '' "" and many and throw a error so use \ before the any non endquates.
+console.log("this is the example of escape \"another value of string\" and \'this is also example of these'")
+
+// templete literals and manipulation (` `)
+let name="heelo";
+let age=34;
+let message = `My name is ${name} and I am ${age} years old.`;
+console.log(
+message
+);
+// without \n add new line
+let poem = `Roses are red,
+Violets are blue,
+JavaScript is fun,
+And so are you.`;
+
+console.log(poem);
