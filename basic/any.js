1c6984a16650a762354ddf0eb0f84dccab9ec5cd
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-09-23 09:33:36 +0000

    this is for basic

---
+++ basic/any.js
@@ -0,0 +1,10 @@
+console.log("well come in the javascript magic world here we are welcome to you")
+
+const makeaobject= {
+    name: "surendar",
+    age: 32,
+    collage: "gitfor",
+    makeking:'forworld'
+};
+
+console.log(makeaobject);
