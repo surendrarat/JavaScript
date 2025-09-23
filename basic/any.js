fb080b57177afa9dfe3a150c43be7c3fb191437a
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-09-23 11:21:19 +0000

    new learning in javascript

--- basic/any.js
+++ basic/any.js
@@ -8,3 +8,22 @@ const makeaobject= {
 };
 
 console.log(makeaobject);
+
+let first=null
+console.log(typeof(first))
+const developer = "Jessica";
+developer[0]
+console.log(developer[1])
+
+let makeNewString='i have a idea for build a world best tech platform for everyone'
+
+console.log(makeNewString.indexOf('build'));
+console.log(makeNewString.indexOf('make'));
+
+
+let makeSecondString=`we are some challanges for doing this type of work `
+console.log(makeSecondString.includes('challanges'))
+console.log(makeSecondString.includes('birthday'))
+
+let considerOtherString="for build these types of system it will take many \nand resorces for making then scalable and effective and more efficent";
+console.log(considerOtherString.slice(10,15));
