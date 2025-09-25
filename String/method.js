83442668c90c184478f908db64b161234f0b8aa5
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-09-25 09:52:25 +0000

    javascript method

---
+++ String/method.js
@@ -0,0 +1,14 @@
+let gurushikar="  it is give the information about the mountaabu and many other thinks "
+
+// let use the many string method on this string 
+
+console.log(gurushikar.replace("is","are"))
+console.log(gurushikar.charCodeAt('g'))
+console.log(gurushikar.indexOf('give'))
+console.log(gurushikar.includes('about'))
+console.log(gurushikar.repeat(2))
+console.log(gurushikar.length-1)
+console.log(gurushikar.slice(9,17))
+
+console.log(gurushikar.trim())
+console.log(gurushikar.trimStart())
